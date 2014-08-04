23933fcf499273dd90caeb456a6c291f1e350e3c
Gregory Boissinot <gregory.boissinot@gmail.com>
2014-08-04 21:07:03 +0200

    Initial Git migration from SVN Jenkins repo

---
+++ dtkit-frmk/dtkit-metrics-util/src/test/java/org/jenkinsci/lib/dtkit/util/converter/ConversionText.java
@@ -0,0 +1,17 @@
+package org.jenkinsci.lib.dtkit.util.converter;
+
+import org.junit.Test;
+
+
+public class ConversionText extends AbstractTest {
+
+    @Test
+    public void convertTxt() throws Exception {
+        convertAndValidate("myex-txt.xsl", "myex.xml", "myex-outtxt.txt");
+    }
+
+    @Test
+    public void convertXml() throws Exception {
+        convertAndValidate("myex-xml.xsl", "myex.xml", "myex-outxml.xml");
+    }
+}
---
+++ dtkit-frmk/dtkit-metrics-util/src/test/resources/org/jenkinsci/lib/dtkit/util/converter/myex-xml.xsl
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+        >
+
+    <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="yes"/>
+
+    <xsl:template match="myex">
+        <root>
+            <xsl:value-of select="@attr1"/>
+        </root>
+    </xsl:template>
+
+</xsl:stylesheet>
