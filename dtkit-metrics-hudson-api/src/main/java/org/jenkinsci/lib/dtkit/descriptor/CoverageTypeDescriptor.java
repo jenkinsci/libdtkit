b98ab9d0487c30109ef39956db9acdc85e2e1c0f
Nikolas Falco <nfalco79@hotmail.com>
2019-05-11 17:43:27 +0200

    CQI Add checkstyle and fix file license header

--- dtkit-metrics-hudson-api/src/main/java/org/jenkinsci/lib/dtkit/descriptor/CoverageTypeDescriptor.java
+++ dtkit-metrics-hudson-api/src/main/java/org/jenkinsci/lib/dtkit/descriptor/CoverageTypeDescriptor.java
@@ -1,32 +1,32 @@
-/*******************************************************************************
- * Copyright (c) 2010 Thales Corporate Services SAS                             *
- * Author : Gregory Boissinot                                                   *
- *                                                                              *
- * Permission is hereby granted, free of charge, to any person obtaining a copy *
- * of this software and associated documentation files (the "Software"), to deal*
- * in the Software without restriction, including without limitation the rights *
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    *
- * copies of the Software, and to permit persons to whom the Software is        *
- * furnished to do so, subject to the following conditions:                     *
- *                                                                              *
- * The above copyright notice and this permission notice shall be included in   *
- * all copies or substantial portions of the Software.                          *
- *                                                                              *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  *
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,*
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    *
- * THE SOFTWARE.                                                                *
- *******************************************************************************/
-
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2010, Thales Corporate Services SAS, Gregory Boissinot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.jenkinsci.lib.dtkit.descriptor;
 
 
 import hudson.DescriptorExtensionList;
 import hudson.model.Descriptor;
-import hudson.model.Hudson;
+import jenkins.model.Jenkins;
 import org.jenkinsci.lib.dtkit.model.InputMetric;
 import org.jenkinsci.lib.dtkit.model.InputMetricException;
 import org.jenkinsci.lib.dtkit.model.InputMetricFactory;
@@ -43,9 +43,10 @@ public abstract class CoverageTypeDescriptor<T extends CoverageType> extends Des
     }
 
     public static DescriptorExtensionList<CoverageType, CoverageTypeDescriptor<?>> all() {
-        return Hudson.getInstance().<CoverageType, CoverageTypeDescriptor<?>>getDescriptorList(CoverageType.class);
+        return Jenkins.get().<CoverageType, CoverageTypeDescriptor<?>>getDescriptorList(CoverageType.class);
     }
 
+    @Override
     public String getDisplayName() {
         return getInputMetric().getLabel();
     }
--- dtkit-metrics-model/src/main/java/org/jenkinsci/lib/dtkit/model/InputMetric.java
+++ dtkit-metrics-model/src/main/java/org/jenkinsci/lib/dtkit/model/InputMetric.java
@@ -1,26 +1,26 @@
-/*******************************************************************************
- * Copyright (c) 2010 Thales Corporate Services SAS                             *
- * Author : Gregory Boissinot                                                   *
- *                                                                              *
- * Permission is hereby granted, free of charge, to any person obtaining a copy *
- * of this software and associated documentation files (the "Software"), to deal*
- * in the Software without restriction, including without limitation the rights *
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    *
- * copies of the Software, and to permit persons to whom the Software is        *
- * furnished to do so, subject to the following conditions:                     *
- *                                                                              *
- * The above copyright notice and this permission notice shall be included in   *
- * all copies or substantial portions of the Software.                          *
- *                                                                              *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  *
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,*
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    *
- * THE SOFTWARE.                                                                *
- *******************************************************************************/
-
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2010, Thales Corporate Services SAS, Gregory Boissinot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.jenkinsci.lib.dtkit.model;
 
 import java.io.File;
@@ -219,14 +219,6 @@ public abstract class InputMetric implements Serializable {
         this.outputValidationErrors = outputValidationErrors;
     }
 
-    /**
-     * --------------------------------------------------------
-     * <p/>
-     * SETTERS for JAX-RS Layer
-     * <p/>
-     * --------------------------------------------------------
-     */
-
     public void setToolName(String toolName) {
         this.toolName = toolName;
     }
@@ -254,14 +246,6 @@ public abstract class InputMetric implements Serializable {
     public void setOutputFormatType(OutputMetric outputFormatType) {
         this.outputFormatType = outputFormatType;
     }
-
-    /**
-     * --------------------------------------------------------
-     * <p/>
-     * HASHCODE() AND EQUALS() for comparaison
-     * <p/>
-     * --------------------------------------------------------
-     */
 
     @Override
     public boolean equals(Object o) {
